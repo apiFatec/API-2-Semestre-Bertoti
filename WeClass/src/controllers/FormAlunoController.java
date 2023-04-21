efbc03e6dd18abde7c86098c618879eaccfedb03
Mateus de Sousa <mateus69107@gmail.com>
2023-04-21 01:50:59 -0300

    Implementação de controllers e navegação

--- WeClass/src/controllers/FormAlunoController.java
+++ WeClass/src/controllers/FormAlunoController.java
@@ -4,8 +4,13 @@
  */
 package controllers;
 
+import dao.AlunoDao;
+import dao.TurmaDao;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +18,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import models.Aluno;
+import models.Turma;
 
 /**
  * FXML Controller class
@@ -25,7 +31,7 @@ public class FormAlunoController implements Initializable {
     private Button btnCriar;
 
     @FXML
-    private ComboBox<?> cbTurma;
+    private ComboBox<Turma> cbTurma;
 
     @FXML
     private TextField txtNome;
@@ -37,15 +43,30 @@ public class FormAlunoController implements Initializable {
     void btnCriar(ActionEvent event) {
         String nome = txtNome.getText();
         int ra = Integer.parseInt(txtRa.getText());
+        int id = cbTurma.getValue().getIdTurma();
         
+        Aluno aluno = new Aluno(ra,nome, id);
+        AlunoDao dao = new AlunoDao();
+        dao.adicionarAluno(aluno);
+        
+        txtNome.setText("");
+        txtRa.setText("");
     }
-
+    
+    ObservableList<Turma> list;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        listaTurma();
+        cbTurma.setItems(list);
     }    
-    
+    public void listaTurma(){
+        TurmaDao dao = new TurmaDao();
+        ArrayList<Turma> l = new ArrayList<>();
+        l = dao.listTurma();
+        this.list = FXCollections.observableArrayList(l);
+    }
 }
